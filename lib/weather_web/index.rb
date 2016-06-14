57d722cae69c3e9dbc807d19d0518060bdac4223
nickmandev <nickmandev@gmail.com>
2016-06-14 23:53:12 +0300

    Everything's working now working on new features and still working on front-end

--- lib/weather_web/index.rb
+++ lib/weather_web/index.rb
@@ -1,39 +1,60 @@
 module WeatherWeb
   class Index < Sinatra::Base
+
+   attr_accessor :data, :errors
+
+   register Sinatra::StaticAssets
+
+   register Sinatra::Reloader
+
    enable :sessions
-    register Sinatra::StaticAssets
-    register Sinatra::Reloader
-    set :root => File.dirname(__FILE__)
-    set :public_folder => File.join(root + '/public')
 
-    get '/' do
-      erb :index
-    end
+   set :root => File.dirname(__FILE__)
 
-    post '/result' do
-      data = WeatherWeb::Data.new
-      data.get_city_id(params[:city])
-      session[:result] = data.results
-      if data.results.length > 1
-        redirect ('/multiple_results')
+   set :public_folder => File.join(root + '/public')
+
+   set :show_exceptions, :after_handler
+
+
+      get '/' do
+        erb :index
       end
-      data.request_data
-      erb :result
-    end
 
-    get '/multiple_results' do
-      session[:result]
-      erb :multiple_results
-    end
+      post '/result' do
+        @data = WeatherWeb::Data.new
+        @data.get_city_id(params[:city])
+        if params[:city].length == 0
+          @errors = "ERROR(Enter a city name) To get back click on Index"
+          session[:error] = @errors
+          redirect '/error'
+        end
+        session[:result] = @data.results
+        if @data.results.length > 1
+          redirect ('/multiple_results')
+        end
+        if @data.request_data.nil?
+          @errors = "There's something wrong with the connection please try again later!"
+          session[:error] = @errors
+          redirect '/error'
+        end
+        erb :result
+      end
 
-    post '/multiple_results' do
-      data = WeatherWeb::Data.new
-      data.multiple_results(params[:_id])
-      data.request_data
-      erb :result
-    end
+      get '/multiple_results' do
+        session[:result]
+        erb :multiple_results
+      end
 
+      post '/multiple_results' do
+        @data = WeatherWeb::Data.new
+        @data.multiple_results(params[:_id])
+        @data.request_data
+        erb :result
+      end
 
+      get '/error' do
+        session[:error]
+        erb :error
+      end
   end
-
 end
