9a376fa358f649ea3d507f60cd36904cf51e70a7
Nick <nickmandev@gmail.com>
2016-11-16 18:26:40 +0200

    Installing Bootstrap and still working on routes.

--- lib/weather_web/public/javascript/app/src/App.vue
+++ lib/weather_web/public/javascript/app/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <div id="app">
-      <router-link to="/logon">Logon</router-link>
-    <p>Hello and welcome to our website. Here you can check the weather on almost every city in the planet.
-    You can create an account and add cities that you frequently check into your Favorites. Our website
-    gives you the data you want via <a href="http://openweathermap.org/" target="_blank">OpenWeather</a>-API.
-    You can check it out their api is free of charge and good for experimenting.</p>
-  </div>
+    <div id="app">
+
+        <div>
+            <router-link to="/login">Logon</router-link>
+                <p>Hello and welcome to our website. Here you can check the weather on almost every city in the planet.
+                You can create an account and add cities that you frequently check into your Favorites. Our website
+                gives you the data you want via <a href="http://openweathermap.org/" target="_blank">OpenWeather</a>-API.
+                You can check it out their api is free of charge and good for experimenting.</p>
+        </div>
+        <div>
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
   name: 'app'
+
 }
 </script>
 
---
+++ lib/weather_web/public/javascript/app/src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="login">
+        <p>Test</p>
+        {{ message }}
+    </div>
+</template>
+<style>
+    body{
+        background-color: white;
+    }
+</style>
+<script>
+    export default{
+        name: 'login',
+        data: {
+            message: 'Try out'
+            }
+    }
+</script>
